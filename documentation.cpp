74f01f71d26ef50d06a3fb104248630a614aba03
lakers4sho <regin.mallari@gmail.com>
2015-03-13 22:10:46 -0700

    Create documentation.cpp

---
+++ documentation.cpp
@@ -0,0 +1,20 @@
+/**
+
+  @mainpage SpaceInvaders Documentation
+  @author R.J. Mallari 304004856
+  @date March 13, 2015
+
+  This is the source code for SpaceInvaders, the game which I have created for Stephen deSalvo's PIC10c class project. Included are headers and source
+  files for the following classes:
+
+    MainWindow (Main menu) \n
+    Instructions \n
+    Bullet \n
+    Enemy \n
+    Game \n
+    Health \n
+    Player \n
+    QuitWidget \n
+    Score \n
+
+  */
